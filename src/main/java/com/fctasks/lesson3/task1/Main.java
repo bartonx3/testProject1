ed39e935c9651e6592a3fbe15d7cc9978702026a
Bartosz Markowski <bartosz.markowski98@gmail.com>
2023-10-04 21:16:07 +0200

    class Cinema added

--- src/main/java/com/fctasks/lesson3/task1/Main.java
+++ src/main/java/com/fctasks/lesson3/task1/Main.java
@@ -3,6 +3,8 @@ package com.fctasks.lesson3.task1;
 public class Main {
     public static void main(String[] args) {
         Cinema cinema1 = new Cinema(1,12,"free");
+        Cinema cinema2 = new Cinema(1,22,"not available");
         System.out.println(cinema1.placeStatus());
+        System.out.println(cinema2.placeStatus());
     }
 }
